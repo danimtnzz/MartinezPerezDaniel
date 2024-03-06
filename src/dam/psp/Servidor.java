513468d429c57e9c495cd40c2a396216a9e7bc9d
danimtnz <danimtnz2004@gmail.com>
2024-03-06 16:38:30 +0100

    funcionan la mayoria menos los de cert

--- src/dam/psp/Servidor.java
+++ src/dam/psp/Servidor.java
@@ -1,9 +1,218 @@
 package dam.psp;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.Base64;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public class Servidor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static KeyStore ks;
+
+	public static void main(String[] args) throws IOException {
+		ExecutorService executorService = Executors.newFixedThreadPool(100);
+		try (ServerSocket sSocket = new ServerSocket(9000)) {
+			ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			ks.load(Servidor.class.getResourceAsStream("/keystore.p12"), "practicas".toCharArray());
+			System.out.println("Server listening on port 9000");
+			while (true) {
+				Socket cliente = sSocket.accept();
+				System.out.println("cliente conectado: " + cliente.getInetAddress());
+				cliente.setSoTimeout(5000);
+				executorService.execute(() -> {
+					try {
+						manejarPeticion(cliente);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+			}
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			e.printStackTrace();
+		} finally {
+			executorService.shutdown();
+		}
+	}
+
+	// funcion utilizada en los métodos para enviar al cliente los errores
+	private static void enviarWriteUTF(Socket cliente, String solicitud) {
+		try {
+			new DataOutputStream(cliente.getOutputStream()).writeUTF(solicitud);
+		} catch (SocketTimeoutException e) {
+			try {
+				new DataOutputStream(cliente.getOutputStream()).writeUTF("ERROR:Read timed out");
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void manejarPeticion(Socket cliente) throws IOException {
+		DataInputStream in = new DataInputStream(cliente.getInputStream());
+		try {
+			String solicitud = in.readUTF();
+			if (solicitud != null && solicitud.startsWith("hash")) {
+				manejarHash(in, cliente);
+			} else if (solicitud != null && solicitud.startsWith("cert")) {
+				manejarCert(in, cliente);
+			} else if (solicitud != null && solicitud.startsWith("cifrar")) {
+				manejarCifrar(in, cliente);
+			} else {
+				enviarWriteUTF(cliente, "ERROR:'" + solicitud + "' no se reconoce como una petición válida");
+			}
+		} catch (SocketTimeoutException e) {
+			enviarWriteUTF(cliente, "ERROR:Read timed out");
+		} catch (EOFException e) {
+			enviarWriteUTF(cliente, "ERROR:Se esperaba una petición");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void manejarHash(DataInputStream in, Socket cliente) {
+		try {
+			MessageDigest md;
+			String algoritmo = in.readUTF();
+			md = MessageDigest.getInstance(algoritmo);
+			byte[] bytes = in.readAllBytes();
+			if (bytes.length > 0) {
+				String cadena = Base64.getEncoder().encodeToString(md.digest(bytes));
+				enviarWriteUTF(cliente, "OK:" + cadena);
+			} else
+				enviarWriteUTF(cliente, "ERROR:Se esperaban datos");
+		} catch (SocketTimeoutException e) {
+			enviarWriteUTF(cliente, "ERROR:Read timed out");
+		} catch (EOFException e) {
+			enviarWriteUTF(cliente, "ERROR:Se esperaba un algoritmo");
+		} catch (IOException | NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void manejarCert(DataInputStream in, Socket cliente) {
+		try {
+			String respuestaEncriptadaBase64 = in.readUTF();
+			byte[] respuestaEncriptadaBytes = Base64.getDecoder().decode(respuestaEncriptadaBase64);
+			String respuestaDecodificada = new String(respuestaEncriptadaBytes, StandardCharsets.UTF_8);
+			if (!respuestaDecodificada.startsWith("OK:")) {
+				enviarWriteUTF(cliente, "ERROR:Respuesta mal formada");
+				return;
+			}
+			String hashBase64 = respuestaDecodificada.substring(3);
+			String solicitud = in.readUTF();
+			if (!solicitud.startsWith("cert:")) {
+				enviarWriteUTF(cliente, "ERROR:Solicitud mal formada");
+				return;
+			}
+			String[] partes = solicitud.split(":");
+			String alias = partes[1];
+			String certificadoBase64 = partes[2];
+			if (alias == null || alias.isEmpty()) {
+				enviarWriteUTF(cliente, "ERROR:Se esperaba un alias");
+				return;
+			}
+			if (certificadoBase64 == null || certificadoBase64.isEmpty()) {
+				enviarWriteUTF(cliente, "ERROR:Se esperaba un certificado");
+				return;
+			}
+			try {
+				CertificateFactory f = CertificateFactory.getInstance("X.509");
+				byte[] certificadoBytes = Base64.getDecoder().decode(certificadoBase64);
+				Certificate cert = f.generateCertificate(new ByteArrayInputStream(certificadoBytes));
+
+				Servidor.ks.setCertificateEntry(alias, cert);
+
+				MessageDigest md = MessageDigest.getInstance("SHA-256");
+				md.update(certificadoBase64.getBytes());
+				String hashCalculado = Base64.getEncoder().encodeToString(md.digest());
+
+				if (hashCalculado.equals(hashBase64)) {
+					enviarWriteUTF(cliente, "OK");
+				} else {
+					enviarWriteUTF(cliente, "ERROR:Hash no coincide");
+				}
+			} catch (CertificateException e) {
+				enviarWriteUTF(cliente, "ERROR:Se esperaba Base64");
+			} catch (KeyStoreException | NoSuchAlgorithmException e) {
+				enviarWriteUTF(cliente, "ERROR:Error en el KeyStore o algoritmo no encontrado");
+			}
+		} catch (IOException e) {
+			enviarWriteUTF(cliente, "ERROR:Error de entrada/salida");
+		}
+	}
+
+	private static void manejarCifrar(DataInputStream in, Socket cliente) {
+		String alias = "";
+		try {
+			alias = in.readUTF();
+			Certificate cert = Servidor.ks.getCertificate(alias);
+			if (cert == null)
+				enviarWriteUTF(cliente, "ERROR:'" + alias + "' no es un certificado");
+			else {
+				Cipher c = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+				c.init(Cipher.ENCRYPT_MODE, cert.getPublicKey());
+				int n;
+				byte[] bloque = new byte[256];
+				DataOutputStream out = new DataOutputStream(cliente.getOutputStream());
+				int contador = 0;
+				while ((n = in.read(bloque)) != -1) {
+					contador++;
+					byte[] cifrado = c.doFinal(bloque, 0, n);
+					out.writeUTF("OK:" + Base64.getEncoder().encodeToString(cifrado));
+				}
+				if (contador == 0) {
+					enviarWriteUTF(cliente, "ERROR:Se esperaban datos");
+				}
+			}
+		} catch (SocketTimeoutException e) {
+			enviarWriteUTF(cliente, "ERROR:Read timed out");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (EOFException e) {
+			enviarWriteUTF(cliente, "ERROR:Se esperaba un alias");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			enviarWriteUTF(cliente, "ERROR:'" + alias + "' no contiene una clave RSA");
+		}
 
 	}
 
--- test/dam/psp/ServidorUnitTest.java
+++ test/dam/psp/ServidorUnitTest.java
@@ -1,4 +1,5 @@
 package dam.psp;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -25,28 +26,28 @@ import org.junit.jupiter.api.TestMethodOrder;
 class ServidorUnitTest {
 
 	static KeyStore ks;
-	
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		ks = KeyStore.getInstance(KeyStore.getDefaultType());
 		ks.load(ServidorUnitTest.class.getResourceAsStream("/keystore.p12"), "practicas".toCharArray());
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) No se envía petición (A)")
 	void test01() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) No se envía petición (B)")
 	void test02() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
 			socket.shutdownOutput();
 			assertEquals("ERROR:Se esperaba una petición", new DataInputStream(socket.getInputStream()).readUTF());
@@ -54,45 +55,47 @@ class ServidorUnitTest {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición incorrecta (A)")
 	void test03() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("abcd");
-			assertEquals("ERROR:'abcd' no se reconoce como una petición válida", new DataInputStream(socket.getInputStream()).readUTF());
+			assertEquals("ERROR:'abcd' no se reconoce como una petición válida",
+					new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición incorrecta (B)")
 	void test04() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("abcd");
 			socket.shutdownOutput();
-			assertEquals("ERROR:'abcd' no se reconoce como una petición válida", new DataInputStream(socket.getInputStream()).readUTF());
+			assertEquals("ERROR:'abcd' no se reconoce como una petición válida",
+					new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(1,4 puntos) Petición \"hash\"")
 	void test05() {
 		String mensaje = "MENSAJE DE PRUEBA";
-		for (String algoritmo: new String [] {"SHA-256", "MD5", "SHA3-512"}) {
-			try (Socket socket = new Socket("localhost", 9000)){
+		for (String algoritmo : new String[] { "SHA-256", "MD5", "SHA3-512" }) {
+			try (Socket socket = new Socket("localhost", 9000)) {
 				socket.setSoTimeout(1000);
 				MessageDigest md;
 				md = MessageDigest.getInstance(algoritmo);
 				String hashB64 = Base64.getEncoder().encodeToString(md.digest(mensaje.getBytes()));
-				
+
 				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 				out.writeUTF("hash");
 				out.writeUTF(algoritmo);
@@ -104,290 +107,311 @@ class ServidorUnitTest {
 			}
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"hash\" sin algoritmo (A)")
 	void test06() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("hash");
-			
+
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"hash\" sin algoritmo (B)")
 	void test07() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("hash");
 			socket.shutdownOutput();
-			
+
 			assertEquals("ERROR:Se esperaba un algoritmo", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"hash\" sin datos (A)")
 	void test08() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("hash");
 			out.writeUTF("SHA-256");
-			
+
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"hash\" sin datos (B)")
 	void test09() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("hash");
 			out.writeUTF("SHA-256");
 			socket.shutdownOutput();
-			
+
 			assertEquals("ERROR:Se esperaban datos", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"hash\" cliente no envía EOF")
 	void test10() {
 		String mensaje = "MENSAJE DE PRUEBA";
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("hash");
 			out.writeUTF("SHA-256");
 			out.write(mensaje.getBytes());
-			
+
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(1,4 puntos) Petición \"cert\"")
 	void test11() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(1000);
 			String b64 = Base64.getEncoder().encodeToString(ks.getCertificate("psp").getEncoded());
 			MessageDigest md;
 			md = MessageDigest.getInstance("SHA-256");
 			md.update(b64.getBytes());
 			String b64HashB64 = Base64.getEncoder().encodeToString(md.digest());
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cert");
 			out.writeUTF("psp");
 			out.writeUTF(b64);
 			socket.shutdownOutput();
-			
+
 			assertEquals("OK:" + b64HashB64, new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException | CertificateEncodingException | NoSuchAlgorithmException | KeyStoreException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cert\" sin alias (A)")
 	void test12() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cert");
-			
+
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
-	}	
-	
+	}
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cert\" sin alias (B)")
 	void test13() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cert");
 			socket.shutdownOutput();
-			
+
 			assertEquals("ERROR:Se esperaba un alias", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cert\" sin certificado (A)")
 	void test14() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cert");
 			out.writeUTF("psp");
-			
+
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cert\" sin certificado (B)")
 	void test15() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cert");
 			out.writeUTF("psp");
 			socket.shutdownOutput();
-			
+
 			assertEquals("ERROR:Se esperaba un certificado", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cert\" envía datos sin codificar en Base64 (B)")
 	void test16() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cert");
 			out.writeUTF("psp");
 			out.writeUTF("*****");
-			
+
 			assertEquals("ERROR:Se esperaba Base64", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(3 puntos) Petición \"cifrar\"")
 	void test17() {
-		fail("Not yet implemented");
+	    String textEjemplo = "Texto para cifrar";
+	    String resultadoCifrado = "";
+	    try (Socket socket = new Socket("localhost", 9000)) {
+	        socket.setSoTimeout(10000);
+	        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+	        dos.writeUTF("cifrar");
+	        dos.writeUTF("psp");
+	        dos.write(textEjemplo.getBytes());
+	        socket.shutdownOutput();
+
+	        DataInputStream in = new DataInputStream(socket.getInputStream());
+
+	        String respuesta = in.readUTF();
+
+	        String[] partesRespuesta = respuesta.split(":");
+	            resultadoCifrado = partesRespuesta[1];
+	            System.out.println(resultadoCifrado);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cifrar\" sin alias (A)")
 	void test18() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cifrar");
-			
+
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cifrar\" sin alias (B)")
 	void test19() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cifrar");
 			socket.shutdownOutput();
-			
+
 			assertEquals("ERROR:Se esperaba un alias", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cifrar\" el alias no es válido")
 	void test20() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			String texto = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme ...";
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cifrar");
 			out.writeUTF("aliasnoválido");
 			out.write(texto.getBytes());
 			socket.shutdownOutput();
-			
-			assertEquals("ERROR:'aliasnoválido' no es un certificado", new DataInputStream(socket.getInputStream()).readUTF());
+
+			assertEquals("ERROR:'aliasnoválido' no es un certificado",
+					new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cifrar\" el certificado no contiene una clave RSA")
 	void test21() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(1000);
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cert");
 			out.writeUTF("alumno");
 			out.writeUTF(Base64.getEncoder().encodeToString(ks.getCertificate("alumno").getEncoded()));
-			
+
 			new DataInputStream(socket.getInputStream()).readUTF();
 		} catch (IOException | CertificateEncodingException | KeyStoreException e) {
 			fail(e.getLocalizedMessage());
 		}
-		
-		try (Socket socket = new Socket("localhost", 9000)){
+
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			String texto = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme ...";
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cifrar");
 			out.writeUTF("alumno");
 			out.write(texto.getBytes());
 			socket.shutdownOutput();
-			
-			assertEquals("ERROR:'alumno' no contiene una clave RSA", new DataInputStream(socket.getInputStream()).readUTF());
+
+			assertEquals("ERROR:'alumno' no contiene una clave RSA",
+					new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cifrar\" cliente no envía EOF")
 	void test22() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			String texto = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme ...";
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cifrar");
 			out.writeUTF("psp");
@@ -405,38 +429,38 @@ class ServidorUnitTest {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cifrar\" no se envían datos (A)")
 	void test23() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cifrar");
 			out.writeUTF("psp");
-			
+
 			assertEquals("ERROR:Read timed out", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
-	
+
 	@Test
 	@DisplayName("(0,2 puntos) Petición \"cifrar\" no se envían datos (B)")
 	void test24() {
-		try (Socket socket = new Socket("localhost", 9000)){
+		try (Socket socket = new Socket("localhost", 9000)) {
 			socket.setSoTimeout(10000);
-			
+
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			out.writeUTF("cifrar");
 			out.writeUTF("psp");
 			socket.shutdownOutput();
-			
+
 			assertEquals("ERROR:Se esperaban datos", new DataInputStream(socket.getInputStream()).readUTF());
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
 
-} 
+}
